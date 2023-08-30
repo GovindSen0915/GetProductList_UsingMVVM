b87aac09f9c7dbcfcd68f4e7837f470f0c3602d0
Govind Sen <govindsen0915@gmail.com>
2023-08-30 13:13:08 +0530

    chore(): add product view controller and product view model file

---
+++ GetProductList/GetProductList/New Group/Screens/Product/View/ProductListViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ProductListViewController.swift
+//  GetProductList
+//
+//  Created by Govind Sen on 30/08/23.
+//
+
+import UIKit
+
+class ProductListViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+
+    }
+  
+
+}
---
+++ GetProductList/GetProductList/New Group/Screens/Product/ViewModel/ProductViewModel.swift
@@ -0,0 +1,12 @@
+//
+//  ProductViewModel.swift
+//  GetProductList
+//
+//  Created by Govind Sen on 30/08/23.
+//
+
+import Foundation
+
+final class ProductViewModel {
+    
+}
