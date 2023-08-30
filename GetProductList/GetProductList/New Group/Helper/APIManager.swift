4823d0e12c36182bd2a6f58421c3a6e5e36248c3
Govind Sen <govindsen0915@gmail.com>
2023-08-30 11:52:59 +0530

    feat(): add api manager file and create singleton class in it

---
+++ GetProductList/GetProductList/New Group/Helper/APIManager.swift
@@ -0,0 +1,19 @@
+//
+//  APIManager.swift
+//  GetProductList
+//
+//  Created by Govind Sen on 30/08/23.
+//
+
+import UIKit
+
+// Singleton Design Pattern
+final class APIManager {
+    
+    static let shared = APIManager()
+    private init() {}
+    
+    func fetchProducts() {
+        
+    }
+}
