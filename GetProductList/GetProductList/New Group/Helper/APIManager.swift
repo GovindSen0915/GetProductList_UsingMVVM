dc9acc5a85ea32dceeddfb3d79ea411ebadfb67d
Govind Sen <govindsen0915@gmail.com>
2023-08-30 12:16:21 +0530

    feat(): api call using json decoder in progress

--- GetProductList/GetProductList/New Group/Helper/APIManager.swift
+++ GetProductList/GetProductList/New Group/Helper/APIManager.swift
@@ -13,7 +13,20 @@ final class APIManager {
     static let shared = APIManager()
     private init() {}
     
-    func fetchProducts() {
-        
+    func fetchProducts(handler: @escaping (Result<[Product], Error>) -> Void) {
+        guard let url = URL(string: Constant.API.productURL) else { return }
+        URLSession.shared.dataTask(with: url) { data, response, error in
+            
+            guard let data, error == nil else { return }
+            
+            guard let response = response as? HTTPURLResponse,
+                  200...299 ~= response.statusCode else { return }
+            
+            do {
+                let products = try JSONDecoder().decode([Product].self, from: data)
+            } catch {
+                
+            }
+        }.resume()
     }
 }
