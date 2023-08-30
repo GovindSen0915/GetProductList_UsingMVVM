584216c36b9591f742cce48db55a5135a3079683
Govind Sen <govindsen0915@gmail.com>
2023-08-30 14:03:40 +0530

    feat(): add data binding using closure between view model and view controller

--- GetProductList/GetProductList/New Group/Screens/Product/View/ProductListViewController.swift
+++ GetProductList/GetProductList/New Group/Screens/Product/View/ProductListViewController.swift
@@ -8,12 +8,47 @@
 import UIKit
 
 class ProductListViewController: UIViewController {
+    
+    private var viewModel = ProductViewModel()
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        self.configuration()
         
 
     }
   
 
+}
+
+extension ProductListViewController {
+    
+    func configuration() {
+        self.initViewModel()
+        self.observeEvent()
+    }
+    
+    func initViewModel() {
+        self.viewModel.fetchProducts()
+    }
+    
+    // Observes data binding event
+    func observeEvent() {
+        self.viewModel.eventHandler = { [weak self] event in
+            guard let self else { return }
+            
+            switch event {
+            case .loading:
+                print("Product loading...")
+            case .stopLoading:
+                print("Stop loading...")
+            case .dataLoaded:
+                print("Data loaded...")
+                print(self.viewModel.products)
+            case .error(let error):
+                print(error)
+            }
+        }
+        
+    }
 }
--- GetProductList/GetProductList/New Group/Screens/Product/ViewModel/ProductViewModel.swift
+++ GetProductList/GetProductList/New Group/Screens/Product/ViewModel/ProductViewModel.swift
@@ -9,4 +9,30 @@ import Foundation
 
 final class ProductViewModel {
     
+    var products: [Product] = []
+    var eventHandler: ((_ event: Event) -> Void)? //  Data Binding using Closure
+    
+    func fetchProducts() {
+        self.eventHandler?(.loading)
+        APIManager.shared.fetchProducts { response in
+            self.eventHandler?(.stopLoading)
+            switch response {
+            case .success(let products):
+                self.products = products
+                self.eventHandler?(.dataLoaded)
+            case .failure(let error):
+                self.eventHandler?(.error(error))
+            }
+        }
+    }
+}
+
+extension ProductViewModel {
+    
+    enum Event {
+        case loading
+        case stopLoading
+        case dataLoaded
+        case error(Error?)
+    }
 }
